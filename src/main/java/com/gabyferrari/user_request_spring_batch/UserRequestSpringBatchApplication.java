8e121b7cd8b9c72614c9171ff94ae21125ada072
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-17 02:31:59 -0300

    Project created

---
+++ src/main/java/com/gabyferrari/user_request_spring_batch/UserRequestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.user_request_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabyferrari/user_request_spring_batch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.user_request_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
